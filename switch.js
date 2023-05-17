d24129385b1a82ab3df42317cd8a5c5d20270f23
Winnie12396 <71814998+Winnie12396@users.noreply.github.com>
2023-05-17 17:15:16 +0800

    Update switch.js

--- switch.js
+++ switch.js
@@ -1,4 +1,4 @@
-<script type="module">
+//<script type="module">
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   import { initializeApp } from "/firebase/app";
@@ -78,4 +78,4 @@
     return Math.floor(Math.random() * max) + min;
   }
 
-</script>
+//</script>
